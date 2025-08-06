dfac66d0a5a1deac4cc789d79b0f9efa5e4585c8
nann5an1 <nangsuhtetsan94@gmail.com>
2025-08-06 15:33:53 +0800

    Initial Commit

---
+++ immersion/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  immersion
+//
+//  Created by Event on 6/8/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ immersion/immersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  immersionApp.swift
+//  immersion
+//
+//  Created by Event on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct immersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
